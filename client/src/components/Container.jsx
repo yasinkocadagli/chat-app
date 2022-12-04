4196ce03749dd88d3ebd54a40a227a77b6582f44
yasin <yasinkocadagli@gmail.com>
2022-12-04 15:32:35 +0300

    backend connection

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+
+import React, { useEffect } from 'react'
 import ChatForm from './ChatForm'
 import ChatList from './ChatList'
+import {init} from "../socketApi"
 
 function Container() {
+
+  useEffect(()=>{
+    init()
+
+  },[])
   return (
     <div className='App'>
         <ChatList/>
